20c23fdb869e923e95c998931812ade5e152418f
Sammy Fung <sammy@sammy.hk>
2021-10-21 11:54:57 +0800

    add sponsor to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer(){
     return (
         <Grid container id="footer">
           <Grid item xs={12}>
-            由<a href="https://freehkfonts.opensource.hk">自由香港字型計劃</a>提供以開放源碼授權發佈的中文免費字型，<a href="https://opensource.hk">開源香港</a>開發<a href="https://github.com/opensourcehk/kaidemo">本示範程式</a>。
+            由<a href="https://freehkfonts.opensource.hk">自由香港字型計劃</a>提供以開放源碼授權發佈的中文免費字型，<a href="https://opensource.hk">開源香港</a>開發<a href="https://github.com/opensourcehk/kaidemo">本示範程式</a>。感謝<a href="https://www.udomain.hk">UDomain</a>贊助雲端服務。
           </Grid>
         </Grid>
     );
